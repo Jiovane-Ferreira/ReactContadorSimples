13c1ff861df23970d39d0a1cf5625a2739a373e8
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-04-14 00:41:36 -0300

    funcionalidades ON

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -5,15 +5,28 @@ function Contador() {
     const [numero, setNumero] = useState(0);
 
 
+    const adicionarNumero = () => {
+        setNumero((prevNumero) => prevNumero + 1);
+    }
+
+    const zerarNumero = () => {
+        setNumero((prevNumero) => prevNumero = 0);
+    }
+
+    const subtrairNumero = () => {
+        setNumero((prevNumero) => prevNumero - 1);
+    }
+
+
     return (
         <div>
             <div className="showValor">
                 Aqui vai o valor: {numero}
             </div>
             <div className="btn-container">
-                <button className="btn-adicionar" onClick={() => setNumero(numero + 1)}>+</button>
-                <button className="btn-zerar" onClick={() => setNumero(0)}>0</button>
-                <button className="btn-subtrair" onClick={() => setNumero(numero - 1)}>-</button>
+                <button className="btn-adicionar" onClick={adicionarNumero}>+</button>
+                <button className="btn-zerar" onClick={zerarNumero}>0</button>
+                <button className="btn-subtrair" onClick={subtrairNumero}>-</button>
             </div> 
         </div>
     )
